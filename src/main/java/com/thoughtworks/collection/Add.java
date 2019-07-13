21301e4b0d73c6154bb5e908a623a82cef632104
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-13 16:56:53 +0800

    complete Add.java

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,11 +2,24 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int leftborder=leftBorder < rightBorder ? leftBorder:rightBorder;
+        int rightborder=leftborder!=leftBorder? leftBorder:rightBorder;
+        int sum=0;
+        List<Integer>list=new ArrayList<>();
+        for(int i=leftborder;i<=rightborder;i++){
+           if(i%2==0){
+                //sum+=i;
+               list.add(i);
+           }
+        }
+        return list.stream().mapToInt(i->i).reduce(0,(a,b)->a+b);
+       // return  sum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
@@ -14,11 +27,11 @@ public class Add {
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(item->item*3+2).reduce(0,(a,b)->a+b);
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map((item)->item%2==0? item:item*3+2).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
@@ -30,7 +43,7 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(item->item%2==0).mapToDouble(item->item).average().orElse(0);
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
@@ -38,7 +51,7 @@ public class Add {
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(item->item%2==0).distinct().collect(Collectors.toList());
     }
 
 //    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
